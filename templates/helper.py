383c1ee979314e6599e88f0fcdd1f2e820c5a9e3
Annique Rozenberg <anniquerozenberg@hotmail.com>
2024-04-26 14:18:05 +0200

    functies toegevoegd

--- templates/helper.py
+++
@@ -1,7 +0,0 @@
-tekst="header"
-lengte = len(tekst) +4
-print ()
-print (lengte * "*")
-print (f"* {tekst} *")
-print(lengte * "*")
-print ()
--- tijdelijk.py
+++ tijdelijk.py
@@ -1,15 +1,27 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5 
-    }
-aanbieding = 0.8 * prijzen ["aardbei"] 
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs 1 liter - slechts € {aanbieding}"
-reclame_tekst2 = reclame_tekst [:61]
-reclame_tekst3 = reclame_tekst2.upper()
-reclame_tekst4 = reclame_tekst3.split(" ") 
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el.upper())
-    else:
-        print(el.lower())
+from helper import decoreer
+
+def print_aanbieding ():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5 
+        }
+    
+    aanbieding = 0.8 * prijzen ["aardbei"] 
+    
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs 1 liter - slechts € {aanbieding}"
+    
+    reclame_tekst2 = reclame_tekst [:61]
+    
+    reclame_tekst3 = reclame_tekst2.upper()
+    
+    reclame_tekst4 = reclame_tekst3.split(" ") 
+    
+    for el in reclame_tekst4:
+        if len(el) >= 5:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding ()
